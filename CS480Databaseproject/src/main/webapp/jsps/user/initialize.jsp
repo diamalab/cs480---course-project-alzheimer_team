0cfbf513040143e914b3163c2792a931a82bef7b
Herve Chendjou <Herve Chendjou@DESKTOP-LOV2QJU>
2021-07-31 20:27:33 -0500

    commit this

--- CS480Databaseproject/src/main/webapp/jsps/user/initialize.jsp
+++ CS480Databaseproject/src/main/webapp/jsps/user/initialize.jsp
@@ -17,7 +17,6 @@
 	<!--
 	<link rel="stylesheet" type="text/css" href="styles.css">
 	-->
-
   </head>
   
   <body>
--- CS480Databaseproject/src/main/webapp/sql/user.sql
+++ CS480Databaseproject/src/main/webapp/sql/user.sql
@@ -4,8 +4,7 @@ CREATE DATABASE alzheimer_team;
 USE  alzheimer_team;
 
 CREATE TABLE tb_user 
-(
-  
+( 
   username VARCHAR(50) primary key,
   `password` VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL
